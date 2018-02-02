8875905f08b155ff3adaa9a08e42440856954b3c
csampedroromero <csampedroromero@victoria26.danielcastelao.org>
2018-02-02 12:41:59 +0100

    Revert "Limpio."
    This reverts commit 7011e505fec9176d71672923d27f29f83d6b64ba.

--- src/boletin52/Boletin52.java
+++ src/boletin52/Boletin52.java
@@ -1,9 +1,16 @@
-
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package boletin52;
 
 import java.util.Scanner;
 
-
+/**
+ *
+ * @author oracle
+ */
 public class Boletin52 {
     Scanner e= new Scanner(System.in);
     short a,b;
