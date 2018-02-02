ccb54f76cb8c1d5384b45f899b7c2b9a55856f0a
csampedroromero <csampedroromero@victoria26.danielcastelao.org>
2018-02-02 13:12:50 +0100

    Acabado el 53.

--- src/boletin53/Boletin53.java
+++ src/boletin53/Boletin53.java
@@ -1,6 +1,37 @@
 
 package boletin53;
 
+import java.util.Scanner;
+
 public class Boletin53 {
-    int a;
+    Scanner fad= new Scanner(System.in);
+    byte a;
+    public void operacion(){
+    a= fad.nextByte();
+    if(a>0){
+        System.out.println("+");
+    }
+    
+    else {
+        if(a<0){
+            System.out.println("-");
+        }
+        else{
+            System.out.println("0");
+        }
+        
+     
+    }
+    
 }
+     public static void main(String[] args) {
+         Boletin53 objeto= new Boletin53();
+         objeto.operacion();
+     }
+    
+    
+}
+
+
+//Teclear un número e sacar por consola o signo “ + “ se o nº é positivo,
+//o signo “ –“ se é negativo e “ 0 “ se é cero .
