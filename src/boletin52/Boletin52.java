d01049b497df40644019bff6fb7a80588062b75d
oracle <oracle@victoria25.danielcastelao.org>
2018-02-02 10:31:13 +0100

    Celtor2.

---
+++ src/boletin52/Boletin52.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin52;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author oracle
+ */
+public class Boletin52 {
+    Scanner e= new Scanner(System.in);
+    short a,b;
+    public void calculo(){
+        System.out.println("Poña o primeiro numero");
+        a=e.nextShort();
+        System.out.println("Poña o segundo numero");
+        b=e.nextShort();
+    System.out.println(a+b);
+        
+    if(a>=b){
+    System.out.println(a-b);
+}
+    }
+     public static void main(String[] args) {
+         Boletin52 r= new Boletin52();
+         r.calculo();
+     }
+    
+}
+//Implementa un programa no que se tecleen dous números de tipo short . 
+//Se o primeiro é maior ou igual que o segundo,visualizaremos a resta .
+//En calquera caso visualizaremos a suma .
