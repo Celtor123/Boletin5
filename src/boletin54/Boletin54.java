d501adbc4f20c305f86c79331b5d2d8cc303ab67
Juan <Juan@DESKTOP-J5PU6RU>
2018-02-03 13:11:06 +0100

    Ahora sí.

--- src/boletin54/Boletin54.java
+++ src/boletin54/Boletin54.java
@@ -12,16 +12,18 @@ public class Boletin54 {
         System.out.println("Inserte os kilos da segunda persoa");
         int d=a.nextInt();
         if(f>d){
-            System.out.println("a persoa que pesa máis é: "+f);
+            System.out.println("a persoa que pesa máis é: "+f+"kg");
             System.out.println("a diferencia é: "+(f-d));
         }
         else{
-            System.out.println("a persoa que peasa máis é: "+d);
+            System.out.println("a persoa que peasa máis é: "+d+"kg");
             System.out.println("a digerencia é: "+(d-f));
-        }
-       
+        }      
     }
-    
+    public static void main(String[] args) {
+        Boletin54 aasd= new Boletin54();
+        aasd.datos();
+    }
 }
 
 //Coñecidos o nome e o peso de dúas persoas. 
